f9ca15b9df0ca33b3bf66dcd901c86efcd3f0257
ramaalm <ramachoudhary2145@gmail.com>
2026-09-09 14:29:04 +0530

    Pre-populate equipment and functional location when creating notification from sub-operation details page

--- ASSETMGRMD250500_0-80009446/Rules/Notifications/CreateUpdate/NotificationCreateChangeSetNav.js
+++ ASSETMGRMD250500_0-80009446/Rules/Notifications/CreateUpdate/NotificationCreateChangeSetNav.js
@@ -50,15 +50,16 @@ export default function NotificationCreateChangeSetNav(context, bindingParams) {
             binding.OnlineEquipment = true;
         }
 
-        /* Changes related to if suboperation have equipment/Funloc when user clicks "Add Notification"
-          in notification screen Equipment & Funlication should come by default started here Ramakishan */
+        /* Changes related to: if suboperation have equipment/Funloc then Pre-populate equipment 
+        and functional location when creating notification from sub-operation details page started here Ramakishan */
           
         else if(contextBinding && contextBinding['@odata.type'] === "#sap_mobile.MyWorkOrderSubOperation"){
             binding.HeaderFunctionLocation = contextBinding.OperationFunctionLocation;
             binding.HeaderEquipment = contextBinding.OperationEquipment;
         }
-        /* Changes related to if suboperation have equipment/Funloc when user clicks "Add Notification"
-          in notification screen Equipment & Funlication should come by default ended here Ramakishan */
+        /* Changes related to: if suboperation have equipment/Funloc then Pre-populate equipment 
+        and functional location when creating notification from sub-operation details page ended here Ramakishan */
+
         let prevPageProxy;
         try {
             if (libCommon.getPageName(context) === 'SideMenuDrawer') {
